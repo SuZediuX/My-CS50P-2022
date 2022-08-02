98c7de56dd8c90e217b2056cc7862ac52ff0dc44
XevoNexuZ <56885709+XevoNexuZ@users.noreply.github.com>
2022-08-02 21:02:44 +0100

    Changed file structure

--- Lecture0/hello.py
+++ Lecture0/hello.py
@@ -8,7 +8,15 @@ print("Hello,", name, sep="...", end="!\n")
 This is a comment
 """
 
+#Don't jump to newline
 print("Hello, ", end="")
+
+#Add a . before newline
 print(name, end=".\n")
 
+#f-String
 print(f'Hi, {name}')
+
+#Trim whitespaces from input string
+name = name.strip()
+print(name)
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # My-CS50P-2022
  Harvard CS50P 2022
+
+ Lecture 0 - Functions, Variables

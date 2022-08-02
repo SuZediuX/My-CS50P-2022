f3a717f4b1335f6e5be98a5bd453b6fa2048792e
XevoNexuZ <56885709+XevoNexuZ@users.noreply.github.com>
2022-08-02 21:45:10 +0100

    String to integer transformation

--- Lecture0/calculator.py
+++ Lecture0/calculator.py
@@ -1,6 +1,13 @@
-x = input("What's x? ")
-y = input("What's y? ")
+x = int(input("What's x?"))
+y = int(input("What's y?"))
 
+print("Result: ", x + y)
+"""
+x = (input("What's x? "))
+y = (input("What's y? "))
+
+#Convert string input to integer type utilizing 'int' function
 z = int(x) + int(y)
 
 print(z)
+"""

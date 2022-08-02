da9f718445af026c30a6801dabf69ae658d88a8d
XevoNexuZ <56885709+XevoNexuZ@users.noreply.github.com>
2022-08-02 21:55:59 +0100

    Float and round off

--- Lecture0/calculator.py
+++ Lecture0/calculator.py
@@ -1,7 +1,15 @@
-x = int(input("What's x?"))
-y = int(input("What's y?"))
+#Integer calculation
+x = int(input("What's x? "))
+y = int(input("What's y? "))
 
 print("Result: ", x + y)
+
+#Float calculation
+a = float(input("What's a? "))
+b = float(input("What's b? "))
+
+c = round(a+b)
+print(f'{c:,}')
 """
 x = (input("What's x? "))
 y = (input("What's y? "))
